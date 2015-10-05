e8aeb6f7f538429a27dfd249b1a9acbfd824d93f
Eivind Bergem <bergem@copyleft.no>
2015-10-05 15:36:21 +0200

    Added comments.

--- findkeyframes/findkeyframes.py
+++ findkeyframes/findkeyframes.py
@@ -2,18 +2,22 @@ import cStringIO, struct, sys
 
 TS_PACKET_SIZE = 188
 
+# Simple wrapper around MPEG TS file
 class MpegTSFile:
     def __init__(self, filename):
         self.fd = open(filename, "rb")
 
+    # Find start of stream by looking for two consequtive TS packets
     def find_sync(self):
         while True:
             pos = self.fd.tell()
 
             x = self.fd.read(1)
 
+            # Look for sync byte
             if x == '\x47':
                 self.fd.seek(-1, 1)
+                # Look for a second packet
                 if self.read_packet()[0] == '\x47':
                     self.fd.seek(pos)
                     break
@@ -23,6 +27,7 @@ class MpegTSFile:
     def read_packet(self):
         return self.fd.read(TS_PACKET_SIZE)
 
+    # Search for substring in file
     def find(self, substr):
         bufsize = TS_PACKET_SIZE*32
         pos = self.fd.tell()
@@ -38,6 +43,7 @@ class MpegTSFile:
                 pos = self.fd.tell()
                 buf = self.fd.read(bufsize)
             else:
+                # Return containing packet
                 pos = pos + buf_pos
                 self.fd.seek(pos - (pos % 188))
                 return self.read_packet()
@@ -53,6 +59,7 @@ class TSPacket:
         self.data = data
         fd = cStringIO.StringIO(data)
 
+        # TS header
         header = struct.unpack('>i', fd.read(4))[0]
         self.header = {"sync_byte": header >> 24,
                        "tei": header >> 23 & 0x1,
@@ -64,11 +71,13 @@ class TSPacket:
                        "cont_payl": header >> 4 & 0x1,
                        "contin": header & 0xf}
 
+        # Adaptation field
         if self.header["adap_field"]:
             length = ord(fd.read(1))
             flags = ord(fd.read(1))
             fd.read(length - 1)
 
+        # Payload
         if self.header['cont_payl']:
             self.stream_id = "\x00"
 
@@ -92,6 +101,7 @@ class Frame:
     def __str__(self):
         return str(self.pos) + ":" + str(self.duration)
 
+# Find keyframes and return array of Frame objects
 def get_keyframes(filename):
     fd = MpegTSFile(filename)
 
@@ -102,19 +112,24 @@ def get_keyframes(filename):
     last_keyframe = None
 
     while True:
+        # Search for beginning of video frame
         data = fd.find("\x00\x00\x01\xe0")
         pos = fd.tell() - TS_PACKET_SIZE
 
         if data:
             packet = TSPacket(data)
 
+            # Check if packet is start of a frame
             if (packet.header["payl_start"] and
                 packet.header["cont_payl"] and
                 packet.stream_id == '\xe0'):
 
+                # Add duration to last keyframe, if set
                 if last_keyframe:
                     last_keyframe.duration += 0.0417
 
+                # Read another packet. keyframe start code is sometimes
+                # in the next packet.
                 data = fd.read_packet()
 
                 if data:
@@ -128,13 +143,16 @@ def get_keyframes(filename):
                     if not x:
                         continue
 
+                    # Look for keyframe start code
                     if (x.payl.find("\x00\x00\x01\x67") != -1 or
                         x.payl.find("\x00\x00\x01\x06") != -1):
                         keyframe = True
                         break
 
                 if keyframe:
+                    # Look for start of next frame
                     if fd.find("\x00\x00\x01\xe0"):
+                        # Include first packet of next frame in size
                         size = fd.tell() - pos
                         fd.seek(-TS_PACKET_SIZE, 1)
                     else:
